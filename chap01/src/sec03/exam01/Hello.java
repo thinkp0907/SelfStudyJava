3293a77d03e463700744ca8bdce5adcbec91a2c6
DongGyun.Cho <thinkp0907@gmail.com>
2021-05-04 19:50:42 +0900

    혼자 공부하는 자바 - chap01

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,15 @@
+package sec03.exam01;
+
+/*
+ 작성자: 조동균
+ 작성일: 2021.05.04
+ 	(범위 주석)
+ */
+
+public class Hello {
+	// 프로그램 실행 진입점		(라인 주석)
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	} // end of main
+} // end of class
