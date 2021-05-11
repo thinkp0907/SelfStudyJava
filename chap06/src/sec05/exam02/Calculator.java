84a8619a54d2066620f9f8bc30dd7105eb19b7d3
DongGyun.Cho <thinkp0907@gmail.com>
2021-05-11 20:05:37 +0900

    혼자서 공부하는 자바 - chap06-05: 인스턴스 멤버와 정적 멤버

---
+++ chap06/src/sec05/exam02/Calculator.java
@@ -0,0 +1,20 @@
+package sec05.exam02;
+
+public class Calculator {
+	
+	// 필드
+	// 정적 필드
+	static double pi = 3.14159;
+	
+	// 생성자
+	
+	// 메소드
+	// 정적 메소드
+	static int plus(int x, int y) {
+		return x + y;
+	}
+	
+	static int minus(int x, int y) {
+		return x - y;
+	}
+}
